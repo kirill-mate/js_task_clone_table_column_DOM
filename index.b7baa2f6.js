20cd99cdb5f61c7bccca46a0ab94392155f9ce55
Kirill <Face2404@ukr.net>
2025-02-06 23:13:07 +0200

    make build

---
+++ index.b7baa2f6.js
@@ -0,0 +1,2 @@
+var r=document.querySelectorAll("tr"),e=!0,l=!1,n=void 0;try{for(var t,a=r[Symbol.iterator]();!(e=(t=a.next()).done);e=!0){var o=t.value;if(5===o.children.length){var d=o.children[1].cloneNode(!0);o.append(d),o.append(o.children[4])}}}catch(r){l=!0,n=r}finally{try{e||null==a.return||a.return()}finally{if(l)throw n}}
+//# sourceMappingURL=index.b7baa2f6.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const rows = document.querySelectorAll("tr");
+for (const row of rows)if (row.children.length === 5) {
+    const clone = row.children[1].cloneNode(true);
+    row.append(clone);
+    row.append(row.children[4]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
